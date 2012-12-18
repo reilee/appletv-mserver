aebd7b14b9115a5b4a1049b13957384d344fc372
styxfisher@gmail.com <styxfisher@gmail.com@20136419-83f1-6bc2-0db2-d6035af3f909>
2012-12-18 07:25:00 +0000

    1 clean temporary .ts file when start server 2 fix seek back 3 optimize seek

--- server/startavs.sh
+++ server/startavs.sh
@@ -4,4 +4,5 @@ export LC_ALL=zh_CN.UTF-8
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/bin:/opt/sbin:.
 export LANG=zh_CN.UTF-8
 cd /opt/avs
+/bin/rm -f /DataVolume/shares/Public/avs/*.ts
 LD_LIBRARY_PATH=/opt/avs/lib python avs.py
